759726a31913ca0d554cfe3f628046ec71979bf5
SlimeMan22 <25941215+SlimeMan22@users.noreply.github.com>
2019-10-06 01:29:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var paste = require("better-pastebin");
 var request = require("sync-request");
 var URL = "https://games.roblox.com/v1/games/3016661674/servers/Public?sortOrder=Asc&limit=100";
-var interval = 1000*15*60;
+var interval = 1000*15*1;
 var DATA = [];
 
 function getServerData(cursor) {
